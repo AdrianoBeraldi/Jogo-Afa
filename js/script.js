f55126ac34bd6596d4f49ba684f73f9b39bee6b9
AdrianoBeraldi <107327131+AdrianoBeraldi@users.noreply.github.com>
2022-06-13 01:50:50 -0300

    Teste commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,37 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 
+const start = () => {
+
+    document.getElementById("text-start").style.color = "rgb(236, 236, 236)";
+
+    pipe.classList.add('pipe-animation');
+
+mario.src = './img/afa.jpeg';
+mario.style.width = '75px'
+mario.style.marginLeft = '50px'
+
+function grassAnimation(){
+    grass.classList.add('grass-animation');
+        }setInterval(grassAnimation, 8000);
+
+
+function floorAnimation1(){
+    floor1.classList.add('floor-animation-1');
+        }setInterval(floorAnimation1, 0);
+
+function floorAnimation2(){
+    floor2.classList.add('floor-animation-2');
+        }setInterval(floorAnimation2, 0);
+                       
+function floorAnimation3(){
+    floor3.classList.add('floor-animation-3');
+        }setInterval(floorAnimation3, 3100); 
+ 
+        
+audioStart.play();
+}
+
 const jump = () => {
     mario.classList.add('jump');
 
